4cab2e6343aad44c6b3e7b8328d487ab62929c98
Erik Hardeman <user.name>
2025-03-05 14:37:44 +0100

    Add new keypair. Add Cognito userpool.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,30 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
 data "aws_vpc" "default" {
   default = true
 }
 
+
+# Network setup
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = "eu-central-1a"
+  availability_zone = var.az1
 }
+
+# Key pair setup
+resource "tls_private_key" "tf_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "tf_keypair" {
+  key_name   = var.keypair_name
+  public_key = tls_private_key.tf_key.public_key_openssh
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.tf_key.private_key_pem
+  sensitive = true
+}
+
+# Security group and rules setup
 resource "aws_security_group" "http_sg" {
   name        = "http_sg"
   description = "Allow HTTP inbound traffic and all outbound traffic"
@@ -42,13 +58,47 @@ resource "aws_security_group_rule" "all_egress" {
   security_group_id = aws_security_group.http_sg.id
 }
 
+# Instance setup
 resource "aws_instance" "ChatInstance" {
-  ami             = "ami-07eef52105e8a2059"
-  instance_type   = "t2.micro"
+  ami             = var.ami
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.http_sg.name]
-  key_name        = "AnsibleKeyPair"
+  key_name        = var.keypair_name
 
   tags = {
     Name = "chat-instance"
   }
+}
+
+output "chat_public_ip" {
+  value     = aws_instance.ChatInstance.public_ip
+  sensitive = false
+}
+
+output "chat_public_dns" {
+  value     = aws_instance.ChatInstance.public_dns
+  sensitive = false
+}
+
+# Cognito configuration
+resource "aws_cognito_user_pool" "chat" {
+  name = "chat-user-pool"
+}
+
+resource "aws_cognito_user_pool_client" "tf_up_client" {
+  name                          = "tf-userpool_client"
+  user_pool_id                  = aws_cognito_user_pool.chat.id
+  supported_identity_providers  = ["COGNITO"]
+  explicit_auth_flows           = ["ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH"]
+  generate_secret               = false
+  prevent_user_existence_errors = "LEGACY"
+  refresh_token_validity        = 1
+  access_token_validity         = 1
+  id_token_validity             = 1
+  token_validity_units {
+    refresh_token = "hours"
+    access_token  = "hours"
+    id_token      = "hours"
+  }
+
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,29 @@
-# variable "pem_file_name" {
-#   description = "Name of the key pair"
-#   type        = string
-#   default     = "/home/erik/awspems/AnsibleKeyPair.pem"
-# }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "az1" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1a"
+}
+
+variable "ami" {
+  description = "AWS AMI string (region specific)"
+  type        = string
+  default     = "ami-07eef52105e8a2059"
+}
+
+variable "instance_type" {
+  description = "AWS EC2 instance type (default is free tier eligible)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "keypair_name" {
+  description = "Name of existing keypair to use in EC2 instance"
+  type        = string
+  default     = "terraform_key_pair"
+}
